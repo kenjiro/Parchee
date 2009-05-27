3160a159e6e2f5b9e261330e68ac312493e989da
kenjiro2u <kenjiro2u@yahoo.co.jp>
2009-05-27 15:57:50 +0900

    create gps method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  # My Page
   def index
+    
   end
 
+  def gps
+    # 位置情報を取得する
+    position = request.mobile.position
+    @latitude = position.lat
+    @longuitude = position.lon
+  end
 end
